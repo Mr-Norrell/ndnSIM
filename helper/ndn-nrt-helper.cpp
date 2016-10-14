17c19727ea035bcb96df01a603455c774a03917d
Mr-Norrell <mhasabet@gmail.com>
2016-10-14 19:24:42 +0330

    helper: add EmptyNetworkRegionTable Refs: #3806

--- helper/ndn-nrt-helper.cpp
+++ helper/ndn-nrt-helper.cpp
@@ -1,4 +1,20 @@
-/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- *//**
+ * Copyright (c) 2011-2016  Regents of the University of California.
+ *
+ * This file is part of ndnSIM. See AUTHORS for complete list of ndnSIM authors and
+ * contributors.
+ *
+ * ndnSIM is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * ndnSIM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ndnSIM, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ **/
 
 #include "ndn-nrt-helper.hpp"
 #include "ns3/ndnSIM/model/ndn-l3-protocol.hpp"
@@ -44,6 +60,25 @@ void
 NetworkRegionTableHelper::RemoveRegionName(NodeContainer &c, const ndn::Name &regionName){
     for(NodeContainer::iterator i = c.begin(); i != c.End(); ++i){
         RemoveRegionName(*i, regionName);
+    }
+}
+
+void
+NetworkRegionTableHelper::EmptyNetworkRegionTable(Ptr<Node> node){
+    NS_LOG_LOGIC("Node [" << node->GetId() << "]$ NetworkRegionTable is cleared");
+
+    Ptr<L3Protocol> l3protocol = node->GetObject<L3Protocol>();
+    NS_ASSERT_MSG(l3protocol != 0, "Ndn stack should be installed on the node");
+
+    node->GetObject<L3Protocol>()->getForwarder()->getNetworkRegionTable().clear();
+
+}
+
+void
+NetworkRegionTableHelper::EmptyNetworkRegionTable(NodeContainer &c){
+
+    for(NodeContainer::iterator i = c.begin(); i != c.End(); ++i){
+        EmptyNetworkRegionTable(*i);
     }
 }
 
--- helper/ndn-nrt-helper.hpp
+++ helper/ndn-nrt-helper.hpp
@@ -1,6 +1,25 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/**
+ * Copyright (c) 2011-2016  Regents of the University of California.
+ *
+ * This file is part of ndnSIM. See AUTHORS for complete list of ndnSIM authors and
+ * contributors.
+ *
+ * ndnSIM is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * ndnSIM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ndnSIM, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
 #ifndef NDN_NRT_HELPER_H
 #define NDN_NRT_HELPER_H
+
 
 
 #include "ns3/node-container.h"
@@ -15,8 +34,8 @@ namespace ndn {
  * @ingroup ndn-helpers
  * @brief Network Region Table Helper
  *
- * The NRT Helper interacts directly with Network Region Table by passing parameters
- * to NRT functions in order to add/remove a prefix to/from the table in one node or
+ * The NRT Helper interacts directly with Network Region Table by using set-like API
+ * in order to add/remove a prefix to/from the table or empty it in one node or
  * a node container
  */
 
@@ -34,6 +53,12 @@ public:
 
     static void
     RemoveRegionName(NodeContainer& c, const Name& regionName);
+
+    static void
+    EmptyNetworkRegionTable(Ptr<Node> node);
+
+    static void
+    EmptyNetworkRegionTable(NodeContainer& c);
 
 
 };
